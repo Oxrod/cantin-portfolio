014f87a39188125b9b6b8788ba52c00b0742f7de
Oxrod <oxrodo@gmail.com>
2023-06-15 14:37:16 +0200

    Default export Photocard

--- pages/components/PhotoCard.jsx
+++ pages/components/PhotoCard.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
-export const PhotoCard = (source) => {
+const PhotoCard = (source) => {
   return (
     <div className="h-full relative">
       <Image src={source} sizes="100%" alt="cardImage" layout="fill"></Image>
     </div>
   );
 };
+
+export default PhotoCard;
